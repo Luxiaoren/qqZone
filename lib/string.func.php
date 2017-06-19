2f174af0f1f71b7776392cf4818e3583aeafd102
Luxiaoren <1622991435@qq.com>
2017-06-19 19:57:42 +0800

    PERFECT THE PAGE SHOW

--- lib/string.func.php
+++ lib/string.func.php
@@ -1,17 +1,6 @@
 <?php
-function buildRandomString($type,$lenght){
-    if($type==1){
-        $chars=join("", range(0,9));
-    }else if($type==2){
-        $chars=join("",array_merge(range("a","z"),range("A","Z")));
-    }elseif($type==3){
-        $chars=join("",array_merge(range("a","z"),range("A","Z"),range(0,9)));
-    }
+function getFriend($str){
+    $arr=explode("|",$str,-1);
+    return $arr;
 
-    if($lenght>strlen($chars)){
-        exit("nono");
-    }
-    $char=str_shuffle($chars);//´òÂÒ×Ö·û´®
-    $chars=substr($char,0,$lenght);//½ØÈ¡×Ö·û´®
-    return $chars;
 }
--- user/mainShow.php
+++ user/mainShow.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once '../data/dbhelper.php';
+require_once '../lib/string.func.php';
 session_start();
 
 $username=$_SESSION['username'];
@@ -9,12 +10,19 @@ $email=$_SESSION['email'];
 
 	$db = new dbhelper();
 	$sql="select user_friend from user_info where id=1";
-
+	//$sql="update user_info set user_friend ='Tom' where id=1";
 	$res = $db->execute_select($sql);
 	$friends=$res[0];
+	
+	$friend=explode('|', $friends);
+	
+	/*$friends.="|james";
+	$sql="update user_info set user_friend ='$friends' where id=1";
+	$rese=$db->execute_insert_updata($sql);*/
 	if($friends==""){
 		$mes="暂无好友";
-		
+	}else{
+		$mes="";
 	}
 
 	
@@ -25,10 +33,33 @@ $email=$_SESSION['email'];
 		<style>
 			
 		</style>
+		<script type="text/javascript">
+			function showAdd(){
+					document.getElementById("button").style.display="none";
+					document.getElementById("button_").style.display="block";
+					document.getElementById("frameOne").style.display="block";
+			}
+		</script>
 	</head>
 <iframe src="./common/head.html" width ="100%" height ="62px" name = "head"  scrolling="no" frameborder="no"></iframe>
-<div id="friendList">好友列表
-	<a><?php ?></a>
+<div id="friendList"><!--好友列表模块-->
+    <a href="#" style='margin-left:30px;'>好友列表</a><br>
+	<?php if($mes==""){
+		
+			for($i=0;$i<sizeof($friend);$i++){
+			echo "<br/><a href='#'style='margin-left:20px;'>·$friend[$i]</a>";
+			}
+		}
+		?>
+</div>
+<div id="selfShow">
 	
+	<input type ="button" value="添加新动态" style="margin-left: 85%;" id ="button" onclick="showAdd()" style="display: block;" />
+	<input type ="button" value="发布" id ="button_" onclick="disAdd()" style="display: none;margin-left: 85%;" />
+	<iframe src="addNew.php" id ="frameOne" width="100%" height="400px" scrolling="no" frameborder="no" style="display: none;"></iframe>
+</div>
+
+<div id="zoneSetting">
+	空间信息设置
 </div>
 </html>

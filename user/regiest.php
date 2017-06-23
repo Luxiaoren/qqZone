0d7b1a41269f05a5ec7a27e29a051cf818962ab3
Luxiaoren <1622991435@qq.com>
2017-06-23 14:29:56 +0800

    regiest

--- user/regiest.php
+++ user/regiest.php
@@ -1,3 +1,23 @@
+
+<?php
+
+
+
+$username=$_COOKIE["username"];
+$password=$_COOKIE["password"];
+$email=$_COOKIE["email"];
+$name=$_COOKIE["name"];
+$idcard=$_COOKIE["idcard"];
+$sex=$_COOKIE["sex"];
+$birth=$_COOKIE["birth"];
+$telno=$_COOKIE["telno"];
+
+
+foreach($_COOKIE as $key=>$val){
+setcookie($key,"",time()-1);
+}
+
+?>
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8">
@@ -8,11 +28,11 @@
 <body>
 	<div id="regiestMain">
 		<form action="regiestProcess.php" method="POST">
-			<table border="1px" style="text-align: center;">
-				<tr><td colspan="2">注册用户</td></tr>
-				<tr><td>用户名：</td><td><input type="text" name="username"/></td></tr>
-				<tr><td>密码：</td><td><input type="text" name="password"/></td></tr>
-				<tr><td>邮箱：</td><td><input type="text" name="email"/></td></tr>
+			<table border="1px" >
+				<tr><td colspan="2" style="text-align: center;">注册用户</td></tr>
+				<tr><td>用户名：</td><td><input type="text" name="username" value="<?php echo "$username";?>" /></td><td><span style="color: red;">*</span></td></tr>
+				<tr><td>密码：</td><td><input type="text" name="password"/></td><td><span style="color: red;">*</span></td></tr>
+				<tr><td>邮箱：</td><td><input type="text" name="email" value="<?php echo $email;?>" /></td><td><span style="color: red;">*</span></td></tr>
 				<tr><td>姓名：</td><td><input type="text" name="name"/></td></tr>
 				<tr><td>身份证号：</td><td><input type="text" name="idcard"/></td></tr>
 				<tr><td>性别：</td><td><input type="radio" name="sex" value="男" />男
--- user/regiestProcess.php
+++ user/regiestProcess.php
@@ -1,7 +1,9 @@
 <?php
 
 header("Content-type:text/html;charset=utf-8");
-require_once '../data/dbhelper.php'
+require_once '../data/dbhelper.php';
+
+
 
 $username=$_POST['username'];
 $password=$_POST['password'];
@@ -12,13 +14,33 @@ $sex=$_POST['sex'];
 $birth=$_POST['birth'];
 $telno=$_POST['telno'];
 
+setcookie("username","$username");
+setcookie("password","$password");
+setcookie("email","$email");
+setcookie("name","$name");
+setcookie("idcard","$idcard");
+setcookie("birth","$birth");
+setcookie("telno","$telno");
+
+if($username==""||$password==""||$email==""){
+	setcookie("error","请填写必须信息");
+	header("Location:regiest.php");
+	die();
+}
+
+
 $db =new dbhelper();
 
-$sql="";
+$sql="insert into user_log(username,password,email)value('$username','$password','$email')";
+echo $sql;
 $res=$db->execute_insert_updata($sql);
 
+if($res==false){
+	echo"no";
+}
+print_r($res);
 
-echo $idcard;
+
 
 
 ?>
